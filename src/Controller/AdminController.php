02f344819ad71662ba6e889a06a6658e7d42742c
Emilie <emilie.chnvt@gmail.com>
2025-04-26 20:08:26 +0200

    promote and demote methods added

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -17,4 +19,29 @@ final class AdminController extends AbstractController
             'users' => $userRepository->findAll(),
         ]);
     }
+
+    #[Route('/promote/{id}', name: 'app_promote')]
+    public function promote(User $user, EntityManagerInterface $entityManager): Response
+    {
+        if(!in_array('ROLE_ADMIN', $user->getRoles() )){
+            $user->setRoles(['ROLE_ADMIN']);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+        }
+        return $this->redirectToRoute('app_admin');
+    }
+
+    #[Route('/demote/{id}', name: 'app_demote')]
+    public function demote(User $user, EntityManagerInterface $entityManager): Response
+    {
+        if(in_array('ROLE_ADMIN', $user->getRoles() )){
+            $user->setRoles([]);
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+        }
+        return $this->redirectToRoute('app_admin');
+    }
+
 }
